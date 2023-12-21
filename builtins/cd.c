e527cea59bdef21797c9c9dc61f59c8a5a5571c3
Paula Biazotto <pds_paula@hotmail.com>
2023-12-21 11:29:42 +0000

    fix: echo read just one flag

--- builtins/cd.c
+++ builtins/cd.c
@@ -1,44 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   echo.c                                             :+:      :+:    :+:   */
+/*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/19 10:36:33 by paula             #+#    #+#             */
-/*   Updated: 2023/12/21 11:26:19 by paula            ###   ########.fr       */
+/*   Created: 2023/12/20 08:28:57 by paula             #+#    #+#             */
+/*   Updated: 2023/12/20 09:18:13 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-static int	ft_check_n(char *args)
-{
-	if (strncmp(args, "-n", 3))
-		return (0);
-	return (1);
-}
-
-int	echo(char **args)
-{
-	int	i;
-	int	new_line;
-
-	i = 1;
-	new_line = 0;
-	if (args[i] && ft_check_n(args[i]))
-	{
-		new_line = 1;
-		i++;
-	}
-	while (args[i])
-	{
-		ft_putstr_fd(args[i], 1);
-		if (args[i + 1])
-			ft_putstr_fd(" ", 1);
-		i++;
-	}
-	if (new_line == 0)
-		ft_putstr_fd("\n", 1);
-	return (0);
-}
