ebd842a147861dc651aa36924318137ffa70f379
Paula Biazotto <pds_paula@hotmail.com>
2024-01-03 16:36:52 +0000

    chore: tomorrow I need copy my env for a array

--- executes/exec_child.c
+++ executes/exec_child.c
@@ -6,16 +6,23 @@
 /*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 15:53:23 by paula             #+#    #+#             */
-/*   Updated: 2024/01/03 15:57:04 by paula            ###   ########.fr       */
+/*   Updated: 2024/01/03 16:31:24 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+// char    *ft_get_path(char *cmd, t_env *my_env)
+// {
+//     char    *path_env;
+//     char    **paths;
+// }
+
 int	ft_exec_child_process(char **args, t_env *my_env)
 {
-	(void)args;
+	// char    *path;
 	(void)my_env;
-	printf("vai ser aqui\n");
+	// path = ft_get_path(args[0], my_env);
+    execv("/usr/bin/ls", args);
 	return (EXIT_SUCCESS);
 }
