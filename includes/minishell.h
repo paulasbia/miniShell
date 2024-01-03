3668462dfd78f6bc972411da4e8381784ffb1c6d
Paula Biazotto <pds_paula@hotmail.com>
2024-01-03 10:13:31 +0000

    chore: refatored executes

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 10:26:33 by paula             #+#    #+#             */
-/*   Updated: 2024/01/02 14:34:06 by paula            ###   ########.fr       */
+/*   Updated: 2024/01/03 10:12:57 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,9 +45,15 @@ void				ft_update_envlist(char *name, char *value, t_env *my_env);
 t_env				*ft_seach_node(char *name, t_env *my_env);
 char				*mini_value(char *name, t_env *my_env);
 
-// builtins
-int					ft_execute_builtin(char **args, t_env **minienv);
+//utils
 int					ft_cmd_builtin(char **args);
+int					str_equal(const char *str1, const char *str2);
+
+// EXECUTES
+int					ft_one_cmd(char *input, t_env **my_env);
+int					ft_execute_builtin(char **args, t_env **minienv);
+
+// builtins
 int					ft_pwd(void);
 int					ft_exit(char **arg, t_env **my_env);
 int					ft_echo(char **args);
@@ -73,6 +79,4 @@ void				ft_print_error_var(char *command, char *var);
 void				ft_exit_with_error(char *command, char *msg, int error);
 int					ft_cd_err_msg(char *err_msg);
 
-// EXECUTES
-int					ft_one_cmd(char *input, t_env **my_env);
 #endif
