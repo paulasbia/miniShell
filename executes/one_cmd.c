7881a27f4ff19c4d0008c902cb558802e843b144
Paula Biazotto <pds_paula@hotmail.com>
2024-01-22 09:46:29 +0000

    feat: added wait status for child process

--- executes/one_cmd.c
+++ executes/one_cmd.c
@@ -6,18 +6,26 @@
 /*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 16:18:04 by paula             #+#    #+#             */
-/*   Updated: 2024/01/21 11:11:02 by paula            ###   ########.fr       */
+/*   Updated: 2024/01/22 09:45:13 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+int	ft_get_exit_status(int status)
+{
+	if (WIFSIGNALED(status))
+		return (128 + WTERMSIG(status));
+	return (WEXITSTATUS(status));
+}
 
 void	back_saved_fd(int saved_fd[2])
 {
 	dup2(saved_fd[1], STDOUT_FILENO);
 	close(saved_fd[1]);
 }
-//verificar o retorno 0 ou 1;
+
+// verificar o retorno 0 ou 1;
 int	ft_execute_child(t_dados *data, t_env *my_env)
 {
 	pid_t	child_pid;
@@ -30,7 +38,7 @@ int	ft_execute_child(t_dados *data, t_env *my_env)
 	if (!child_pid)
 		ft_exec_child_process(data->comando, my_env);
 	waitpid(child_pid, &status, 0);
-	return (WEXITSTATUS(status));
+	return (ft_get_exit_status(status));
 }
 
 // mudar a funcao free para o da Tais aqui
