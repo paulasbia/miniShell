a922f15bfe3bbaf3c83142ecb331c8903a6db597
Paula Biazotto <pds_paula@hotmail.com>
2024-01-23 15:41:51 +0000

    fix: changed validade_input inside a if condition

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ricardo <ricardo@student.42.fr>            +#+  +:+       +#+        */
+/*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 10:09:35 by paula             #+#    #+#             */
-/*   Updated: 2024/01/23 15:14:09 by ricardo          ###   ########.fr       */
+/*   Updated: 2024/01/23 15:41:06 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,15 +39,14 @@ int	minishell(t_env *my_env)
 		if (input[0])
 			add_history(input);
 		// função check args
-		if (validate_input(input) == 1)
+		if (validate_input(input) == 0)
 		{
-			continue;
+			dados = parsing(input); //se tudo tiver ok eu faço o parsing;
+			free_list(&dados);
 		}
-		dados = parsing(input); //se tudo tiver ok eu faço o parsing
 		//exit_status = start_execution(input, &my_env);
-		free_list(&dados);
 		free(input);
-		input = NULL;
+	//	input = NULL;
 	}
 	return (exit_status);
 }
