4860f16e7ff36537216232a06fded6ab5fce6d16
taiscremaschi <taiscremaschi@gmail.com>
2024-01-24 17:16:21 +0000

    wip norma

--- parsing/split_minishell.c
+++ parsing/split_minishell.c
@@ -6,13 +6,13 @@
 /*   By: ricardo <ricardo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 10:56:43 by ricardo           #+#    #+#             */
-/*   Updated: 2024/01/24 16:25:32 by ricardo          ###   ########.fr       */
+/*   Updated: 2024/01/24 16:59:07 by ricardo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	handle_word_quotes(int *i, int *j, char const *s, char c)
+int	handle_word_quotes(int *i, int *j, char const *s, char c)
 {
 	(*i)++;
 	(*j)++;
