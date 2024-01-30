520853d95b0b006284392997336b3c33346bd3a2
Paula Biazotto <pds_paula@hotmail.com>
2024-01-30 19:33:42 +0000

    storing artifact

--- unit/main_teste.c
+++ unit/main_teste.c
@@ -28,8 +28,8 @@ static void assert_n_files_and_clean(char **actuals, char **expecteds)
 		char *join4 = ft_strjoin("rm ", join2);
 	//	return_code = run_cmd((char *[]){"bash", "-c", join3, NULL});
 	//	TEST_ASSERT_EQUAL(0, return_code);
-		return_code = run_cmd((char *[]){"bash", "-c", join4, NULL});
-		TEST_ASSERT_EQUAL(0, return_code);
+	//	return_code = run_cmd((char *[]){"bash", "-c", join4, NULL});
+	//	TEST_ASSERT_EQUAL(0, return_code);
 		actuals++;
 		expecteds++;
 		free(join1);
