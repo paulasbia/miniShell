c3adc5d23913e8e53db46be746b2957804709d55
Paula Biazotto <pds_paula@hotmail.com>
2024-01-31 18:29:23 +0000

    WIP: deleted fake parse

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 10:09:35 by paula             #+#    #+#             */
-/*   Updated: 2024/01/31 18:27:50 by paula            ###   ########.fr       */
+/*   Updated: 2024/01/31 18:28:48 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,6 @@ int	start_execution(t_dados *data, t_env **my_env)
 	else
 		exit_status = ft_execute_multiple_cmd(data, *my_env);
 	return (exit_status);
-}
-
-// um fake apenas para poder executar testes com a entrada t_dados
-t_dados	*fake_parser(char *input)
-{
-	t_dados	*parsed;
-
-	parsed = malloc(sizeof(t_dados));
-	parsed->comando = ft_split(input, ' ');
-	parsed->nbr_redirections = 0;
-	parsed->redirect = NULL;
-	parsed->next = NULL;
-	return (parsed);
 }
 
 int	check_heredoc(t_dados *data, t_dados **data_out, int *red_out)
