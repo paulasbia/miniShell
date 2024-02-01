5df7751a39a9e97ca81e76c4d6138a8e883cbe37
Paula Biazotto <pds_paula@hotmail.com>
2024-02-01 10:02:22 +0000

    WIP: heredoc with fork

--- executes/heredoc.c
+++ executes/heredoc.c
@@ -6,11 +6,29 @@
 /*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 09:42:53 by paula             #+#    #+#             */
-/*   Updated: 2024/02/01 09:43:13 by paula            ###   ########.fr       */
+/*   Updated: 2024/02/01 09:59:59 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+void	define_heredoc_signals(int child_pid)
+{
+	struct sigaction	sa_sigint;
+	struct sigaction	sa_sigquit;
+
+	sa_sigint.sa_flags = 0;
+	sigemptyset(&sa_sigint.sa_mask);
+	if (child_pid == 0)
+		sa_sigint.sa_handler = SIG_DFL;
+	else
+		sa_sigint.sa_handler = SIG_IGN;
+	sigaction(SIGINT, &sa_sigint, NULL);
+	sa_sigquit.sa_flags = 0;
+	sigemptyset(&sa_sigquit.sa_mask);
+	sa_sigquit.sa_handler = SIG_IGN;
+	sigaction(SIGQUIT, &sa_sigquit, NULL);
+}
 
 int	check_heredoc(t_dados *data, t_dados **data_out, int *red_out)
 {
@@ -41,21 +59,35 @@ t_dados	*parse_heredoc(t_dados *dados)
 	t_dados	*temp;
 	int		i;
 	int		fd;
+	pid_t	child_pid;
 
 	if (check_heredoc(dados, &temp, &i))
 	{
-		fd = open("/tmp/heredoc", O_CREAT | O_RDWR | O_TRUNC, 0644);
-		input = readline("> ");
-		while (input && !str_equal(input, temp->redirect[i].filename))
+		child_pid = fork();
+		define_heredoc_signals(child_pid);
+		if (child_pid < 0)
+			ft_child_err("fork - heredoc", temp->redirect->filename);
+		else if (!child_pid)
 		{
-			ft_putstr_fd(input, fd);
-			ft_putstr_fd("\n", fd);
-			free(input);
+			fd = open("/tmp/heredoc", O_CREAT | O_RDWR | O_TRUNC, 0644);
 			input = readline("> ");
+			while (input && !str_equal(input, temp->redirect[i].filename))
+			{
+				ft_putstr_fd(input, fd);
+				ft_putstr_fd("\n", fd);
+				free(input);
+				input = readline("> ");
+			}
+			free(input);
+			close(fd);
+            temp->redirect[i].filename = "/tmp/heredoc";
+			exit(EXIT_SUCCESS);
 		}
-		free(input);
-		close(fd);
-		temp->redirect[i].filename = "/tmp/heredoc";
+        else
+        {
+            ft_wait_exit_status(child_pid);
+            ft_init_signal();
+        }
 	}
 	return (dados);
 }
