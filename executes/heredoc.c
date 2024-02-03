8e8d20799a9a971ee102c5e690322d8c36dc43eb
Paula Biazotto <pds_paula@hotmail.com>
2024-02-03 10:04:03 +0000

    WIP: heredoc worked, without norm

--- executes/heredoc.c
+++ executes/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 09:42:53 by paula             #+#    #+#             */
-/*   Updated: 2024/02/03 09:25:21 by paula            ###   ########.fr       */
+/*   Updated: 2024/02/03 10:03:11 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,43 +30,41 @@ void	define_heredoc_signals(int child_pid)
 	sigaction(SIGQUIT, &sa_sigquit, NULL);
 }
 
-int	check_heredoc(t_dados *data, t_dados **data_out, int *red_out)
+t_redirect	*check_heredoc(t_dados **data, int *red_out)
 {
-	*data_out = NULL;
-	while (data)
+	while (*data)
 	{
-		while (*red_out < data->nbr_redirections)
+		while (*red_out < (*data)->nbr_redirections)
 		{
-			if (data->redirect->redirect_type == 2)
+			if ((*data)->redirect[*red_out].redirect_type == 2)
 			{
-				*data_out = data;
-				return (*data_out != NULL);
+				return (&((*data)->redirect[*red_out]));
 			}
 			(*red_out)++;
 		}
-		data = data->next;
+		*data = (*data)->next;
 		*red_out = 0;
 	}
-	return (*data_out != NULL);
+	return NULL;
 }
 
-void	ft_read_heredoc(t_dados *temp, int *i)
+void	ft_read_heredoc(t_redirect *redirect)
 {
 	char	*input_hd;
 	int		fd_hd;
 
 	fd_hd = open("/tmp/heredoc", O_CREAT | O_RDWR | O_TRUNC, 0644);
 	input_hd = readline("> ");
-	while (input_hd && !str_equal(input_hd, temp->redirect[*i].filename))
+	while (input_hd && !str_equal(input_hd, redirect->filename))
 	{
 		ft_putstr_fd(input_hd, fd_hd);
 		ft_putstr_fd("\n", fd_hd);
 		free(input_hd);
-		input_hd = readline("> ");
+		input_hd = readline("> ");  
 	}
 	if (!input_hd)
 		print_error_msg("warning: heredoc delimited by EOF. Wanted",
-			temp->redirect->filename);
+			redirect->filename);
 	free(input_hd);
 	close(fd_hd);
 	exit(EXIT_SUCCESS);
@@ -74,27 +72,30 @@ void	ft_read_heredoc(t_dados *temp, int *i)
 
 t_dados	*parse_heredoc(t_dados *dados)
 {
-	t_dados	*temp;
-	int		i;
-	pid_t	child_pid;
+	t_dados		*temp;
+	int			i;
+	pid_t		child_pid;
+	t_redirect	*red_temp;
 
+	temp = dados;
 	i = 0;
-	while (check_heredoc(dados, &temp, &i))
+	red_temp = check_heredoc(&temp, &i);
+	while ((red_temp))
 	{
+		i++;
 		child_pid = fork();
 		define_heredoc_signals(child_pid);
 		if (child_pid < 0)
-			ft_child_err("fork - heredoc", temp->redirect->filename);
+			ft_child_err("fork - heredoc", red_temp->filename);
 		else if (!child_pid)
-		{
-			ft_read_heredoc(temp, &i);
-		}
+			ft_read_heredoc(red_temp);
 		else
 		{
 			ft_wait_exit_status(child_pid);
-			free(temp->redirect[i].filename);
-			temp->redirect[i].filename = ft_strdup("/tmp/heredoc");
+			free(red_temp->filename);
+			red_temp->filename = ft_strdup("/tmp/heredoc");
 			ft_init_signal();
+			red_temp = check_heredoc(&temp, &i);
 		}
 	}
 	return (dados);
