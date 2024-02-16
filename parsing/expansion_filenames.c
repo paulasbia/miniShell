068a410af68220ed88141e2b47955c2a97d5615a
taiscremaschi <taiscremaschi@gmail.com>
2024-02-16 12:41:35 +0000

    expansao com files

--- parsing/expansion_filenames.c
+++ parsing/expansion_filenames.c
@@ -6,72 +6,75 @@
 /*   By: ricardo <ricardo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 22:51:02 by ricardo           #+#    #+#             */
-/*   Updated: 2024/02/14 11:19:14 by ricardo          ###   ########.fr       */
+/*   Updated: 2024/02/16 12:22:46 by ricardo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	change_wrong_env_filename(char *filename, char *env_input, int j)
+void	change_wrong_env_filename(t_redirect *red, char *env_input, int j)
 {
 	int	x;
 
 	x = 1;
-	if (filename[0] == '"')
-		x += 2;
-	while (!is_space(filename[j]) && val_chars_env(filename[j]) == 0 && x++)
+	while (!is_space(red->filename[j]) && val_chars_env(red->filename[j]) == 0
+		&& x++)
 		j++;
-	if ((int)ft_strlen(filename) == x)
-	{
-		free(filename);
-		filename = NULL;
-	}
+	if ((int)ft_strlen(red->filename) == x)
+		red->ambiguos = 1;
 	else
-		filename = change_exit(filename, "", (ft_strlen(env_input) + 1));
+		red->filename = change_exit(red->filename, "", (ft_strlen(env_input)
+				+ 1));
 	free(env_input);
 }
 
-int	handle_file(int *j, char **file, t_env *tmp_l_env)
+int	handle_file(int *j, t_redirect *red, t_env *tmp_l_env)
 {
 	int		start;
 	char	*env_input;
 
 	start = *j;
-	while (!is_space((*file)[*j]) && val_chars_env((*file)[*j]) == 0)
+	while (!is_space(red->filename[*j])
+		&& val_chars_env(red->filename[*j]) == 0)
 		(*j)++;
-	env_input = ft_substr((*file), start, (*j - start));
+	env_input = ft_substr(red->filename, start, (*j - start));
 	*j = 0;
-	if (search_env(file, tmp_l_env, env_input) == NULL)
+	if (search_env(&red->filename, tmp_l_env, env_input) == NULL)
 	{
-		change_wrong_env_filename(*file, env_input, start);
+		change_wrong_env_filename(red, env_input, start);
 		return (1);
 	}
 	free(env_input);
 	return (0);
 }
 
-int	command_expansion_filename(char **file, t_env *tmp_l_env,
+int	command_expansion_filename(t_redirect *red, t_env *tmp_l_env,
 		int exit_status)
 {
 	int	j;
 
 	j = 0;
-	while ((*file)[j] != '\0')
+	while (red->filename[j] != '\0')
 	{
-		check_expansion_quotes(*file, &j);
-		if ((*file)[j] == '$')
+		check_expansion_quotes(red->filename, &j);
+		if (red->filename[j] == '$')
 		{
-			if (find_exception(*file, j) == 1)
+			if (find_exception(red->filename, j) == 1)
 				break ;
-			j++;
-			while ((*file)[j] == '$')
+			j++;                            
+			while (red->filename[j] == '$') 
 				j++;
-			if (handle_exit_status(file, j, exit_status) == 1)
+			if (handle_exit_status(&red->filename, j, exit_status) == 1)
+				
 				continue ;
-			if (handle_file(&j, file, tmp_l_env) == 1)
+			if (handle_file(&j, red, tmp_l_env) == 1)
+			{
+				if (red->ambiguos == 1)
+					ft_putstr_fd("ambiguos redirect\n", 2);
 				break ;
+			}
 		}
-		if ((*file)[j] != '\0')
+		if (red->filename[j] != '\0')
 			j++;
 	}
 	return (0);
