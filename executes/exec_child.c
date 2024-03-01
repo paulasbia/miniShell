c57048a67e7f61b9e40b6c08d07809421bb0d77b
Paula Biazotto <pds_paula@hotmail.com>
2024-03-01 13:25:56 +0000

    WIP:SHLVL need norm

--- executes/exec_child.c
+++ executes/exec_child.c
@@ -6,7 +6,7 @@
 /*   By: paula <paula@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 15:53:23 by paula             #+#    #+#             */
-/*   Updated: 2024/02/20 16:09:41 by paula            ###   ########.fr       */
+/*   Updated: 2024/03/01 13:24:08 by paula            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,10 +84,13 @@ int	ft_exec_child_process(t_dados *data, t_env *my_env)
 	char	*path;
 	char	**env_array;
 	char	*value;
+	char	*value_sh;
+	int		new_value;
 
 	path = NULL;
 	ft_check_exit(data, my_env);
 	value = mini_value("PATH", my_env);
+	value_sh = NULL;
 	if (ft_strchr(data->cmd[0], '/') || !value)
 		path = ft_strdup(data->cmd[0]);
 	else if (data->cmd[0][0] != '\0')
@@ -95,8 +98,16 @@ int	ft_exec_child_process(t_dados *data, t_env *my_env)
 	if (path == NULL)
 		external_exit(data, my_env, CMD_NOT_FOUND);
 	rl_clear_history();
+	if (str_equal("./minishell", path))
+	{
+		value_sh = mini_value("SHLVL", my_env);
+		new_value = ft_atoi(value_sh);
+		new_value++;
+		ft_update_envlist("SHLVL", ft_itoa(new_value), my_env);
+	}
 	env_array = myenv_to_array(my_env);
 	ft_free_env(&my_env);
+	fprintf(stderr, "path %s data cmd %s/n", path, data->cmd[0]);
 	if (execve(path, data->cmd, env_array))
 		ft_handle_errors(data, path, env_array);
 	exit(EXIT_SUCCESS);
